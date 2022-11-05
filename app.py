72181d1534c556905f1fe0023f9bed07581be4df
Juanes8a <80339256+Juanes8a@users.noreply.github.com>
2022-11-05 18:44:29 -0500

    Anexo de distintos atributos y textos

--- app.py
+++ app.py
@@ -8,15 +8,17 @@ import math
 dat = pd.read_csv('datos.csv')
 
 
-st.title("Asesor educativo")
+st.title("Findool")
 st.subheader("¡Bienvenido!")
 st.write("La elección de una institución de educación superior es una de las más importantes que se pueda tomar, es el lugar donde se empieza, afianza o se reconstruye nuestras prioridades y nuestro estilo de vida. Por tal motivo se ofrece esta aplicación con la finalidad de ayudar a esta persona que está en búsqueda de una institución de educación superior y que cumpla con algunos criterios que son indispensables en su elección.")
+st.subheader("¿Para quién va dirigida?")
+st.write("Esta aplicación está dirigida para bachilleres que deseen continuar sus estudios en instituciones de educación superior.")
 st.subheader("¿Cómo funciona?")
 st.write("Después de esta explicación encontrarás una serie de criterios, los cuales te pediremos que analices y los ajustes de acuerdo a tus intereses y/o necesidades. Estos filtros que abarcan ámbitos como:")
 mark = """
 * Económico: Costo promedio de la institución. 
 * Geográfico: Búsqueda en un estado en particular o en todo el país. 
-* Educativo: Puntuación en SAT, Tasa de admisiones(Alta-Media-Baja), interes en intersemestrales, índice en becasPell y préstamos federales.
+* Educativo: Puntuación en SAT, Tasa de admisiones, interes en intersemestrales, índice en becasPell y préstamos federales.
 """
 col1, col2 = st.columns(2)
 
@@ -31,7 +33,7 @@ st.write("Luego de ajustar los filtros a tus preferencias le das en aplicar y en
 
 
 # Institución educativa
-tipo = ("Cualquiera", "Privada","Pública")
+tipo = ("Privada","Pública")
 terminOption = st.selectbox(
     "Elige el tipo de institución", tipo)
 # : Costo neto de la institucion
@@ -59,7 +61,25 @@ state = ('Todos','AL', 'AK', 'AZ', 'NM', 'AR', 'CA', 'MN', 'CO', 'CT', 'NY', 'DE
 stateOptions = st.selectbox(
         "Elige el estado por el que desees filtrar o selecciona la opción: Todos si te es indiferente el estado",state)
 
+orden = ('Puntaje SAT','Costo', 'Tasa de Admisión')
+opcion_ordenar = st.selectbox(
+        "Elige la variable prioritaria por la cual deseas ver los resultados ",orden)
 
+diccionario_orden = {'Costo':("NPT4_PRIV","NPT4_PUB"),'Puntaje SAT':"SAT_AVG_ALL", 'Tasa de Admisión':"ADM_RATE_ALL"}
+
+def ordenar (df):
+    if orden == 'Puntaje SAT':
+        final_df = df.sort_values(by=["SAT_AVG_ALL"])
+        return final_df.head(10)
+    elif  orden == 'Tasa de Admisión':
+        final_df = df.sort_values(by=["ADM_RATE_ALL"], ascending=False)
+        return final_df.head(10)
+    elif orden == 'Costo' and terminOption == "Privada":
+        final_df = df.sort_values(by=["NPT4_PRIV"])
+        return final_df.head(10)
+    else:
+        final_df = df.sort_values(by=["NPT4_PUB"])
+        return final_df.head(10) 
 def top ():
     filtrado = dat[(dat["ADM_RATE_ALL"] <= 1)
              & (dat["PCTFLOAN"]>= loanSlider) 
@@ -72,8 +92,10 @@ def top ():
 
     if stateOptions !="Todos":
         filtrado3 = filtrado2[filtrado2["STABBR"] == stateOptions]
-        return filtrado3.head(10)
-    return filtrado2.head(10)
+        
+        
+        return ordenar(filtrado3)
+    return ordenar(filtrado2)
 
 if st.button("Buscar instituciones educativas"):    
     seleccionados = top()
